9ce7a74b3177100266585286a007ed29811d4e0a
kokhinmaungwin <khinmaungwin675@gmail.com>
2025-11-10 12:29:53 +0630

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,18 @@
 const CACHE_NAME = 'ace-editor-pwa-v1';
-const OFFLINE_URL = '/';
+const OFFLINE_URL = './';
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache =>
       cache.addAll([
-        '/',
-        '/index.html',
-        '/app.js',
-        '/manifest.json',
-        '/icons/icon-192.svg',
-        '/icons/icon-512.svg',
-        '/icons/icon-192.png',
-        '/icons/icon-512.png',
+        './',
+        './index.html',
+        './app.js',
+        './manifest.json',
+        './icons/icon-192.svg',
+        './icons/icon-512.svg',
+        './icons/icon-192.png',
+        './icons/icon-512.png',
         'https://cdnjs.cloudflare.com/ajax/libs/ace/1.15.2/ace.js'
       ])
     )
